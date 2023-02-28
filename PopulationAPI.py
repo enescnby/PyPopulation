3ba67134287277c42a7c1f6312b987d07d5fbdca
enescnby <enes1canbay@gmail.com>
2023-02-28 13:31:38 +0300

    Update

--- PopulationAPI.py
+++ PopulationAPI.py
@@ -3,28 +3,52 @@ Population API for 1960-2021 population datas.
 
 Data Source: The World Bank (https://data.worldbank.org/indicator/SP.POP.TOTL)
 """
+import pandas, numpy
 
-import pandas
 
 class PopulationAPI:
+
+    class DataError(Exception):
+
+        def __init__(self, msg) -> None:
+            super().__init__()
+            self.msg = msg
+
+        def __str__(self) -> str:
+            return self.msg
 
     def __init__(self, excel_file_name: str = 'population_data.xls') -> None:
         self.file_name = excel_file_name
         self.dataframe = pandas.read_excel(self.file_name)
 
     def get_population(self, country: str, year: str | int) -> int | None:
-        year = str(year)
+
         try:
-            data_series = self.dataframe.loc[self.dataframe['Country'] == country.lower(), year]
+            int(year)
+        except ValueError:
+            raise ValueError("The year must be an integer.")
+        year = str(year)
+
+        try:
+            data_series = self.dataframe.loc[self.dataframe['Country'] == country.lower(
+            ), year]
         except KeyError:
-            print("There is no data in " + year)
-            return None
+            raise PopulationAPI.DataError("There is no data in " + year)
+
         try:
             data = int(data_series)
         except ValueError:
-            print("There is no data in " + country + " in " + year + " or can not fetching." )
-            return None
+            raise PopulationAPI.DataError(
+                "There is no data in " + country + " in " + year + " or can not fetching.")
         except TypeError:
-            print("Invalid country name.")
-            return None
+            raise NameError("Invalid country name.")
         return data
+
+    def get_population_list(self, country: str, years: tuple | list) -> list:
+
+        data_series = self.dataframe.loc[self.dataframe['Country'] == country.lower(), str(years[0]):str(years[1])].astype(int)
+        return data_series.values.flatten().tolist()
+
+x = PopulationAPI()
+lst = x.get_population_list('Turkiye', (1960,1970))
+print(lst)
